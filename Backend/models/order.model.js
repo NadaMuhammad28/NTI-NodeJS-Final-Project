5b591f1b73e3bcad63e5297801e7b4e0fc86ca28
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-10-25 00:03:23 +0200

    Update order.model.js

--- Backend/models/order.model.js
+++ Backend/models/order.model.js
@@ -3,7 +3,59 @@ const validator = require("validator");
 
 const orderSchema = mongoose.Schema(
   {
-    
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "processing",
+      lowercase: true,
+      enum: ["cancelled", "delivered", "shipped", "processing"]
+    },
+
+    phoneNumber: {
+      type: String,
+      trim: true,
+      required: true,
+      validate: (value) => {
+        if (!validator.isMobilePhone(value, "ar-EG"))
+          throw new Error("invalid phone Number");
+      },
+    },
+    cart: {
+      cartId: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
+      required: true
+    }
+,
+    /* orderItems: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, default: 1, required: true },
+        //price: { type: Number, required: true }
+      },
+    ], */
+
+    addresses: [
+      {
+        addrTye: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        addrDetails: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
