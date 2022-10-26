1ddb2dbf8bdcd9ee879e9fcbe801e4a458419cea
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-10-26 14:45:16 +0200

    Update order.routes.js

--- Backend/routes/order.routes.js
+++ Backend/routes/order.routes.js
@@ -1,8 +1,10 @@
-const router = require("express").Router();
-const Order = require("../controllers/order.controller");
-const { auth, authUser, authAdmin } = require("../middleware/auth.middleware");
-const upload = require("../middleware/fileUpload.middleware");
+const router = require("express").Router()
+const Order= require("../controllers/order.controller")
+const { auth, authUser, authAdmin } = require("../middleware/auth.middleware")
+const upload = require("../middleware/fileUpload.middleware")
 
 router.post("/add", auth, authUser, Order.addOrder);
+router.delete("/deleteOrder/:id",auth,authUser,Order.deleteOrder) 
+router.get("/getOrder/:id", auth, Order.getOrder)
 
-module.exports = router;
+module.exports = router
