f9f5a2946886b1028b10f9661e1fd7a2accb75ec
Nadamuhammad28 <nadamuhammad28@gmail.com>
2022-10-27 14:32:21 +0200

    frontend

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { Error404Component } from './components/pages/error404/error404.component';
+import { ProductsComponent } from './components/shop/products/products.component';
+import { SingleproductComponent } from './components/shop/singleproduct/singleproduct.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent,
+    Error404Component,
+    ProductsComponent,
+    SingleproductComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
